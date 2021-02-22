c08983d136ffdcf972ceb41e50ce159f436af557
Clemens Brunner <clemens.brunner@gmail.com>
2021-02-22 17:15:16 +0100

    Read meta information

--- example_eeg.py
+++ example_eeg.py
@@ -29,18 +29,30 @@ raw.set_eeg_reference("average")
 ica = mne.preprocessing.read_ica(fpath / "S01-ica.fif.gz")
 ica.apply(raw)
 
-# load response times (epoch length)
-durations = np.genfromtxt("/Users/clemens/Downloads/testfiles/S01_RT.csv",
-                          skip_header=1)
-tmax = durations.max()
+# load meta information
+log = pd.read_csv(fpath / "S01_EEG.csv", usecols=["thisItem", "corr"])
+log.columns = ["item", "correct"]
+tmp = log["item"].str.split(expand=True)
+tmp.columns = ["op1", "op", "op2"]
+tmp["op1"] = tmp["op1"].astype(int)
+tmp["op2"] = tmp["op2"].astype(int)
+strategy = pd.read_csv(fpath / "S01_Strategy.csv",
+                       usecols=["thisItem", "strat_Keys"])
+strategy.columns = ["item", "strategy"]
+strategy["strategy"].replace({"num_1": "retrieve", "num_2": "count",
+                              "num_3": "transform", "num_4": "other"},
+                             inplace=True)
+log = log.merge(strategy)
+log.drop(columns="item", inplace=True)
+rt = pd.read_csv(fpath / "S01_RT.csv")
+log = pd.concat((tmp, log, rt), axis="columns")
 
-# load accuracy information
-correct = pd.read_csv(fpath / "S01_EEG.csv")["corr"].values
+tmax = log["rt"].max()
 
 epochs = mne.Epochs(raw, events, event_id=dict(onset=2), tmin=-2, tmax=tmax,
                     baseline=None, reject_by_annotation=False, preload=True)
 
-sel = (durations > 0) & (correct == 0)  # epoch selection
+sel = (log["rt"] > 0) & (log["correct"] == 0)  # epoch selection
 
 # plot classical TFR
 freqs = np.arange(1, 36, 0.5)
@@ -50,5 +62,5 @@ tfr = mne.time_frequency.tfr_multitaper(epochs[sel], freqs=freqs,
 tfr.average().plot(baseline=(None, 0), mode="ratio", dB=True)
 
 # plot time-warped TFR
-tfr_warped = tfr_timewarp(tfr, durations[sel])
+tfr_warped = tfr_timewarp(tfr, log["rt"][sel].values)
 tfr_warped.average().plot(baseline=(None, 0), mode="ratio", dB=True)
