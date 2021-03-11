82f0cf82a3fb45e32971765cf0d9729259d779d5
Clemens Brunner <clemens.brunner@gmail.com>
2021-03-11 11:56:57 +0100

    Use better names

--- timewarp.py
+++ timewarp.py
@@ -9,7 +9,8 @@ def tfr_timewarp(tfr, durations):
     Parameters
     ----------
     tfr : mne.time_frequency.EpochsTFR
-        Precomputed EpochsTFR using fixed-length epochs.
+        Precomputed EpochsTFR using fixed-length epochs. Time-warping is based
+        on the duration of the longest epoch.
     durations : numpy.ndarray
         Duration of each epoch (in s).
 
@@ -18,14 +19,14 @@ def tfr_timewarp(tfr, durations):
     warped : mne.time_frequency.EpochsTFR
         Time-warped EpochsTFR.
     """
-    tstart = np.zeros_like(durations, dtype=int)
-    tstop = np.round(durations * tfr.info["sfreq"]).astype(int) + 1
-    max_samp = np.max(tstop - tstart)
-    data = np.empty((*tfr.data.shape[:-1], max_samp))
+    start = np.zeros_like(durations, dtype=int)
+    stop = np.round(durations * tfr.info["sfreq"]).astype(int) + 1
+    longest = np.max(stop - start)
     baseline = tfr.times < 0
+    data = np.empty((*tfr.data.shape[:-1], longest))
     for i, epoch in enumerate(tfr.data):
-        cropped = epoch[..., np.arange(tstart[i], tstop[i]) + baseline.sum()]
-        data[i] = resample_poly(cropped, up=max_samp, down=cropped.shape[-1],
+        cropped = epoch[..., np.arange(start[i], stop[i]) + baseline.sum()]
+        data[i] = resample_poly(cropped, up=longest, down=cropped.shape[-1],
                                 axis=-1, padtype="line")
     data = np.concatenate((tfr.data[..., baseline], data), axis=-1)
     return EpochsTFR(tfr.info, data, tfr.times[:data.shape[-1]], tfr.freqs)
