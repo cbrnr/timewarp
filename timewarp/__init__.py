8c5e00fc78fb8840c62eb1434c17d1c76e81a17f
Clemens Brunner <clemens.brunner@gmail.com>
2021-07-08 10:41:35 +0200

    Move plot_tfr_grid to module

--- timewarp/__init__.py
+++ timewarp/__init__.py
@@ -1,6 +1,6 @@
 """Time-warp variable-length TFRs."""
 
-from .timewarp import tfr_timewarp, generate_epochs
+from .timewarp import plot_tfr_grid, tfr_timewarp, generate_epochs
 
 
-__all__ = [tfr_timewarp, generate_epochs]
+__all__ = [tfr_timewarp, generate_epochs, plot_tfr_grid]
