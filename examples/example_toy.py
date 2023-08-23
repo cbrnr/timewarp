259412052e0132845920a1d0e9511cdc34cc653e
Clemens Brunner <clemens.brunner@gmail.com>
2023-08-23 11:50:32 +0200

    Minor update to toy example

--- examples/example_toy.py
+++ examples/example_toy.py
@@ -1,9 +1,7 @@
 """Time-warping example using toy data."""
-
 import numpy as np
 from mne.time_frequency import tfr_multitaper
 from timewarp import tfr_timewarp, generate_epochs
-
 
 # generate toy data
 epochs, durations = generate_epochs(baseline=2.5)
@@ -15,9 +13,9 @@ tfr = tfr_multitaper(epochs, freqs=freqs, n_cycles=freqs, average=False, return_
 tfr.average().plot(baseline=(-2.5, -0.5), mode="ratio", title="Classic")
 
 # plot time-warped TFR
-tfr_warped = tfr_timewarp(tfr, durations)
-tfr_warped.average().plot(baseline=(-2.5, -0.5), mode="ratio", title="Time-warped")
+tfr1 = tfr_timewarp(tfr, durations)
+tfr1.average().plot(baseline=(-2.5, -0.5), mode="ratio", title="Time-warped")
 
 # plot time-warped TFR with custom resampling (baseline 1000 and activity 8000 samples)
-tfr_warped1 = tfr_timewarp(tfr, durations, resample=(1000, 8000))
-tfr_warped1.average().plot(baseline=(-1, -0.25), mode="ratio", title="Time-warped (custom)")
+tfr2 = tfr_timewarp(tfr, durations, resample=(1000, 8000))
+tfr2.average().plot(baseline=(-2.5, -0.5), mode="ratio", title="Time-warped (custom)")
