b9133003210e0690b6be87672844344b88f49536
Clemens Brunner <clemens.brunner@gmail.com>
2023-08-23 10:57:59 +0200

    Fix __all__

--- timewarp/__init__.py
+++ timewarp/__init__.py
@@ -5,4 +5,4 @@ from .timewarp import (
 )
 
 
-__all__ = [tfr_timewarp, tfr_timewarp_multichannel, plot_tfr_grid, generate_epochs]
+__all__ = ["tfr_timewarp", "tfr_timewarp_multichannel", "plot_tfr_grid", "generate_epochs"]
