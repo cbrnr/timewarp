f4f50829d10628a8089f9b582ff080f76635ee0a
Clemens Brunner <clemens.brunner@gmail.com>
2024-05-16 10:18:19 +0200

    Simplify EEG example

--- examples/example_eeg.py
+++ examples/example_eeg.py
@@ -1,17 +1,14 @@
 """Time-warping example using real EEG data."""
 
-from pathlib import Path
+# make sure that the working directory is set to the examples folder
 
 import mne
 import numpy as np
 import pandas as pd
 from timewarp import plot_tfr_grid, tfr_timewarp_multichannel
 
-# load data
-fpath = Path("./data")
-raw = mne.io.read_raw_bdf(
-    fpath / "S01.bdf", exclude=[f"EXG{i}" for i in range(1, 9)], preload=True
-)
+# load data (get it from https://osf.io/download/5fhye/)
+raw = mne.io.read_raw_bdf("S01.bdf", exclude=[f"EXG{i}" for i in range(1, 9)], preload=True)
 fs = raw.info["sfreq"]
 events = mne.find_events(raw, uint_cast=True)
 events = events[events[:, 2] == 2, :]  # keep only problem onset events (2)
@@ -19,37 +16,10 @@ events = events[events[:, 2] == 2, :]  # keep only problem onset events (2)
 # preprocess
 raw.drop_channels("Status")
 raw.set_montage("biosemi64")
-bad_segments = np.loadtxt(fpath / "S01_bad_segments.csv", skiprows=1, delimiter=",") / fs
-raw.set_annotations(mne.Annotations(*bad_segments.T, "bad_segment"))
-
-with open(fpath / "S01_bad_channels.csv") as f:
-    raw.info["bads"] = f.read().strip().split(",")
-
 raw.set_eeg_reference("average")
 
-ica = mne.preprocessing.read_ica(fpath / "S01-ica.fif.gz")
-ica.apply(raw)
-
-raw.interpolate_bads()
-
-# load meta information
-log = pd.read_csv(fpath / "S01_EEG.csv", usecols=["thisItem", "corr"])
-log.columns = ["item", "correct"]
-tmp = log["item"].str.split(expand=True)
-tmp.columns = ["op1", "op", "op2"]
-tmp["op1"] = tmp["op1"].astype(int)
-tmp["op2"] = tmp["op2"].astype(int)
-strategy = pd.read_csv(fpath / "S01_Strategy.csv", usecols=["thisItem", "strat_Keys"])
-strategy.columns = ["item", "strategy"]
-strategy["strategy"].replace(
-    {"num_1": "retrieve", "num_2": "procedure", "num_3": "procedure", "num_4": "other"},
-    inplace=True,
-)
-log = log.merge(strategy, how="left")
-log.drop(columns="item", inplace=True)
-rt = pd.read_csv(fpath / "S01_RT.csv")
-metadata = pd.concat((tmp, log, rt), axis="columns")
-
+# load meta data
+metadata = pd.read_csv("metadata.csv")
 tmax = metadata["rt"].max()
 
 epochs = mne.Epochs(
